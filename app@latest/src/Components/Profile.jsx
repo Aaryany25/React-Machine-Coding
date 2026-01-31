973bca6bb74e2b41f3881f4b06b04bfa922be8ec
Aaryan <aaryany2505@gmail.com>
2026-01-31 12:50:35 +0530

    Added Ptrofile Section

--- app@latest/src/Components/Profile.jsx
+++ app@latest/src/Components/Profile.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import Button from './Button'
 
-function Profile() {
+function Profile({data ,setData}) {
+    const {name,age,email} = data
+    const handleSetData = (e,item)=>{
+setData(prev=>({
+    ...prev,
+    [item]:e.target.value,
+
+}))
+    }
   return (
     <div className=' border w-[80%]  rounded-lg p-8 mt-3'>
-      Profile 
+      <div>
+        <label >Name:</label>
+        <input type="text" name="" id="" value={name} onChange={(e)=>handleSetData(e,'name')} className="border ml-2 px-2" />
+      </div>
+        <div>
+        <label >Age:</label>
+        <input type="text" name="" id="" value={age} onChange={(e)=>handleSetData(e,"age")} className="border ml-2 px-2"/>
+      </div>
+        <div>
+        <label >Email:</label>
+        <input type="text" name="" id="" value={email} onChange={(e)=>handleSetData(e,"email") }   className="border ml-2 px-2"
+/>
+      </div>
     </div>
   )
 }
--- app@latest/src/Components/Tab-Form.jsx
+++ app@latest/src/Components/Tab-Form.jsx
@@ -4,6 +4,13 @@ import Profile from './Profile'
 import Interest from './Interest'
 import Setting from './Setting'
 function TabForm() {
+    const [data, setdata] = useState({
+        name:"",
+        age:'',
+        email:"",
+        // interest:[],
+        // theme:"dark"
+    })
     const [Active, setActive] = useState(0)
 const tabs =[
 {name:"Profile",component:Profile},
@@ -22,7 +29,7 @@ const ActiveComponent = tabs[Active].component
 <div>
     {/* {ActiveComponent}
      */}
-     <ActiveComponent/>
+     <ActiveComponent data={data} setData={setdata}/>
 </div>
     </div>
   )
