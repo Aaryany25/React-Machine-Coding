296a8a70d2df00f8aa0c21316f8fa1c1338ce7ec
Aaryan <aaryany2505@gmail.com>
2026-02-18 00:07:24 +0530

    logic for Fetching the data

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Page from './components/page'
 
 function App() {
+  const [products, setproducts] = useState([])
+  
+  const FetchData = async()=>{
+    const data = await fetch("https://dummyjson.com/products")
+    const json = await data.json()
+    setproducts(json.products)
+// json.products
+  }
+  useEffect(() => {
+  
+  FetchData()
+    
+  }, [])
+  
   return (
     <div>
-      {/* <page/> */}
-      <Page/>
+   
     </div>
   )
 }
