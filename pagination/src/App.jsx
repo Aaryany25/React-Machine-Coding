cb02cf1b5ee5c0cbbc9e749e04351dcf525d5c1f
Aaryan <aaryany2505@gmail.com>
2026-02-18 00:59:39 +0530

    Added Product Card

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import Page from './components/page'
-
+const ProductCard =({images,title})=>{
+  return <div className='product-card'>
+    <img src={images} alt={title} />
+    <span>{title}</span>
+  </div>
+}
 function App() {
   const [products, setproducts] = useState([])
   
@@ -16,9 +21,11 @@ function App() {
     
   }, [])
   
-  return (
+  return !products.length ? (<h1>No Products Found</h1>):(
     <div>
-   
+   {
+    products.map(p=><ProductCard key={p.id} images={p.images} title={p.title}/>)
+   }
     </div>
   )
 }
